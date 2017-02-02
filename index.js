7c272ce381dc6ef6dfb546e71cc579de967ef56e
ralusek <ralusek@gmail.com>
2017-02-01 18:00:00 -0800

    added deps.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 
-module.exports.isMap = require('hodash-ismap');
-module.exports.isSet = require('hodash-isset');
+module.exports.flattenToSet = require('hodash.flatten-to-set');
+
+module.exports.isMap = require('hodash.is-map');
+module.exports.isSet = require('hodash.is-set');
